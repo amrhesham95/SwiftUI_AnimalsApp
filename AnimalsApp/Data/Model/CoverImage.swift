fa5e6262e82ebf91f25fc4f8b389efff0e540d86
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-06-16 15:28:21 +0200

    Creating CoverImage Model
    Creating CoverImage Model

---
+++ AnimalsApp/Data/Model/CoverImage.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImage.swift
+//  AnimalsApp
+//
+//  Created by Amr Hesham on 16/06/2021.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
