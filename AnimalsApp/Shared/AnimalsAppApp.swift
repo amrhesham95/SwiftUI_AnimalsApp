5925333dfc2a2b55ae24d62a6690cbf2abd388f6
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-06-16 14:15:55 +0200

    initial commit
    initial commit

---
+++ AnimalsApp/Shared/AnimalsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsAppApp.swift
+//  Shared
+//
+//  Created by Amr Hesham on 16/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
